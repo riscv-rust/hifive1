9263484b2d80fb4ced3e2cd9b3331c12a7050330
David Craven <david@craven.ch>
2017-11-22 11:22:22 +0100

    Document Plic driver.

--- examples/blinky_plic.rs
+++ examples/blinky_plic.rs
@@ -2,46 +2,43 @@
 
 extern crate hifive;
 
-use hifive::{interrupt, led, Red, Blue, Green, Interrupt, Plic,
-             Channel, Pwm, Peripherals};
+use core::fmt::Write;
+use hifive::*;
+use hifive::prelude::*;
+use hifive::interrupt::Nr;
 
 fn main() {
-    let peripherals = hifive::init(115_200);
-    led::init(peripherals.GPIO0);
+    let p = hifive::init(115_200);
+    led::init(p.GPIO0);
 
-    let pwm = Pwm(peripherals.PWM0);
-    pwm.init();
-    pwm.set_cmp(Channel::_0, u16::max_value());
-    pwm.set_cmp(Channel::_1, 0);
-    pwm.set_cmp(Channel::_2, u16::max_value() / 2);
+    Red::on(p.GPIO0);
 
-    let plic = Plic(peripherals.PLIC);
-    plic.enable(Interrupt::PWM0CMP0);
-    plic.enable(Interrupt::PWM0CMP1);
-    plic.enable(Interrupt::PWM0CMP2);
-    plic.enable(Interrupt::PWM0CMP3);
+    let plic = Plic(p.PLIC);
+    plic.init();
+
+    RtcConf::new().end(p.RTC);
+    Rtc(p.RTC).set_timeout(500.ms());
+
+    plic.set_priority(Interrupt::RTC, Priority::P7);
+    plic.enable(Interrupt::RTC);
+
+    let serial = Serial(p.UART0);
+    let mut stdout = Port(&serial);
+    writeln!(stdout, "RTC interrupt is set {}", plic.is_enabled(Interrupt::RTC)).unwrap();
+    writeln!(stdout, "RTC interrupt nr is {}", Interrupt::RTC.nr()).unwrap();
 
     unsafe {
         interrupt::enable();
     }
+
+    loop {}
 }
 
 #[no_mangle]
 pub fn plic_trap_handler(p: &Peripherals, intr: &Interrupt) {
-    //let pwm = Pwm(p.PWM0);
-
     match *intr {
-        Interrupt::PWM0CMP0 => {
+        Interrupt::RTC => {
             Blue::toggle(p.GPIO0);
-        },
-        Interrupt::PWM0CMP1 => {
-            Green::toggle(p.GPIO0);
-        },
-        Interrupt::PWM0CMP2 => {
-            Red::toggle(p.GPIO0);
-        },
-        Interrupt::PWM0CMP3 => {
-
         },
         _ => {},
     }
--- src/plic.rs
+++ src/plic.rs
@@ -3,14 +3,18 @@ use riscv::interrupt::Nr;
 use e310x::PLIC;
 pub use e310x::Interrupt;
 
+/// Priority of a plic::Interrupt.
+#[derive(Clone, Copy)]
 pub enum Priority {
-    Never, P1, P2, P3, P4, P5, P6, P7,
+    P0, P1, P2, P3, P4, P5, P6, P7,
 }
 
 impl Priority {
-    pub fn from(prio: u32) -> Priority {
+    /// Takes a read interrupt priority or plic threshold
+    /// register value and returns a plic::Priority enum.
+    fn from(prio: u32) -> Priority {
         match prio {
-            0 => Priority::Never,
+            0 => Priority::P0,
             1 => Priority::P1,
             2 => Priority::P2,
             3 => Priority::P3,
@@ -24,9 +28,11 @@ impl Priority {
 }
 
 impl Into<u32> for Priority {
+    /// Returns the numeric priority for wirting to a
+    /// interrupt priority or the plic threshold register.
     fn into(self) -> u32 {
         match self {
-            Priority::Never => 0,
+            Priority::P0 => 0,
             Priority::P1 => 1,
             Priority::P2 => 2,
             Priority::P3 => 3,
@@ -38,6 +44,7 @@ impl Into<u32> for Priority {
     }
 }
 
+/// Plic interface
 pub struct Plic<'a>(pub &'a PLIC);
 
 impl<'a> Clone for Plic<'a> {
@@ -49,78 +56,127 @@ impl<'a> Clone for Plic<'a> {
 impl<'a> Copy for Plic<'a> {
 }
 
+/// Represents a register offset and mask for
+/// accessing an individual bit in a register
+/// file.
+struct Loc {
+    offset: usize,
+    mask: u32,
+}
+
+impl Loc {
+    /// Computes the location of an interrupt.
+    //#[inline]
+    pub fn from(intr: Interrupt) -> Self {
+        // offset = nr / 32
+        // bit = nr % 32
+        // 32 = 2 ^ 5
+        let nr = intr.nr();
+        let bit = nr & 31;
+        Self {
+            offset: (nr >> 5) as usize,
+            mask: 1 << bit
+        }
+    }
+
+    /// Checks if bit is set.
+    //#[inline]
+    pub fn is_set(&self, bits: u32) -> bool {
+        bits & self.mask == self.mask
+    }
+}
+
 impl<'a> Plic<'a> {
-    /*pub fn init(&self) {
+    /// Initializes PLIC controller by resetting all
+    /// enable bits to 0 and enables MachineExternal
+    /// interrupts.
+    pub fn init(&self) {
         for reg in self.0.enable.iter() {
             unsafe {
                 reg.write(|w| w.bits(0));
             }
         }
+        self.set_threshold(Priority::P0);
+        self.enable_mext();
+    }
 
-        //interrupt!(MachineExternal, plic::plic_handler());
-    }*/
-
-    pub fn enable_mexternal(&self) {
+    /// Enable MachineExternal interrupts.
+    #[inline]
+    pub fn enable_mext(&self) {
         csr::mie.set(|w| w.mext());
     }
 
-    pub fn clear_mexternal(&self) {
+    /// Disable MachineExternal interrupts.
+    #[inline]
+    pub fn disable_mext(&self) {
         csr::mie.clear(|w| w.mext());
     }
 
+    /// Returns true when plic::Interrupt is pending.
     pub fn is_pending(&self, intr: Interrupt) -> bool {
-        let mask = 1 << (intr.nr() % 32);
-        let pending = self.0.pending[(intr.nr() / 32) as usize].read();
-        pending.bits() & mask == mask
+        let loc = Loc::from(intr);
+        let pending = self.0.pending[loc.offset].read();
+        loc.is_set(pending.bits())
     }
 
+    /// Returns true when plic::Interrupt is enabled.
     pub fn is_enabled(&self, intr: Interrupt) -> bool {
-        let mask = 1 << (intr.nr() % 32);
-        let enable = self.0.enable[(intr.nr() / 32) as usize].read();
-        enable.bits() & mask == mask
+        let loc = Loc::from(intr);
+        let enable = self.0.enable[loc.offset].read();
+        loc.is_set(enable.bits())
     }
 
+    /// Enables plic::Interrupt.
     pub fn enable(&self, intr: Interrupt) {
-        let mask = 1 << (intr.nr() % 32);
+        let loc = Loc::from(intr);
         unsafe {
-            self.0.enable[(intr.nr() / 32) as usize]
-                .modify(|r, w| w.bits(r.bits() | mask));
+            self.0.enable[loc.offset]
+                .modify(|r, w| w.bits(r.bits() | loc.mask));
         }
     }
 
+    /// Disables plic::Interrupt.
     pub fn disable(&self, intr: Interrupt) {
-        let mask = 1 << (intr.nr() % 32);
+        let loc = Loc::from(intr);
         unsafe {
-            self.0.enable[(intr.nr() / 32) as usize]
-                .modify(|r, w| w.bits(r.bits() & !mask));
+            self.0.enable[loc.offset]
+                .modify(|r, w| w.bits(r.bits() & !loc.mask));
         }
     }
 
+    /// Claims the plic::Interrupt with the highest priority.
     pub fn claim(&self) -> Interrupt {
         Interrupt::from(self.0.claim.read().bits() as u8)
     }
 
+    /// Notifies the PLIC that the claimed plic::Interrupt is
+    /// complete.
     pub fn complete(&self, intr: Interrupt) {
         unsafe {
             self.0.claim.write(|w| w.bits(intr.nr() as u32));
         }
     }
 
+    /// Returns the plic::Priority of a plic::Interrupt.
     pub fn get_priority(&self, intr: Interrupt) -> Priority {
-        Priority::from(self.0.priority[(intr.nr() - 1) as usize].read().bits())
+        Priority::from(self.0.priority[intr.nr() as usize].read().bits())
     }
 
+    /// Sets the plic::Priority of a plic::Interrupt.
     pub fn set_priority(&self, intr: Interrupt, prio: Priority) {
         unsafe {
-            self.0.priority[(intr.nr() - 1) as usize]
+            self.0.priority[intr.nr() as usize]
                 .write(|w| w.bits(prio.into()));
         }
     }
 
+    /// Returns the PLIC threshold priority.
     pub fn get_threshold(&self) -> Priority {
         Priority::from(self.0.threshold.read().bits())
     }
 
+    /// Sets the PLIC threshold priority. This disables all
+    /// interrupts with a lower plic::Priority.
     pub fn set_threshold(&self, prio: Priority) {
         unsafe {
             self.0.threshold.write(|w| w.bits(prio.into()));
