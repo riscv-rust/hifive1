c05ba7ed00ab5bb63a7a5650d87d9ddc212f04f0
Vadim Kaushan <admin@disasm.info>
2019-02-28 22:11:19 +0300

    CI: build with features enabled

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,8 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
+    cargo check --target $TARGET --features 'board-hifive1'
+    cargo check --target $TARGET --features 'board-lofive'
 }
 
 main
