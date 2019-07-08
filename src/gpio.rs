6b5c354c6a63fdc0955324a4ea01c69dfaa28d45
Ales Katona <almindor@gmail.com>
2019-07-08 15:33:14 -0600

    serial -> uart, add numbering to spi and i2c

--- src/gpio.rs
+++ src/gpio.rs
@@ -3,15 +3,15 @@
 /// 
 /// # Mappings
 /// 
-///   - `spi_<x>` — SPI pins where `<x>` is one of (`sck`, `mosi`, `miso`, `ss0`, `ss2`, `ss3`)
-///   - `i2c_<x>` — I2C pins where `<x>` is one of (`sda`, `scl`)
-///   - `serial_<x>` — Serial pins where <x> is one of (`tx`, `rx`)
+///   - `spi0_<x>` — SPI pins where `<x>` is one of (`sck`, `mosi`, `miso`, `ss0`, `ss2`, `ss3`)
+///   - `i2c0_<x>` — I2C pins where `<x>` is one of (`sda`, `scl`)
+///   - `uart0_<x>` — Serial pins where <x> is one of (`tx`, `rx`)
 ///   - `dig#` — Digital/physical pins on the board where `#` is from range 0..19
 /// 
 /// # Example
 /// 
 /// ```
-/// let mosi = pin!(gpio, spi_mosi); // gpio.pin3
+/// let mosi = pin!(gpio, spi0_mosi); // gpio.pin3
 /// ```
 /// 
 #[macro_export]
@@ -19,19 +19,19 @@ macro_rules! pin {
     // empty
     ($gpio:ident, none) => { () };
     // spi
-    ($gpio:ident, spi_sck) => { $gpio.pin5 };
-    ($gpio:ident, spi_mosi) => { $gpio.pin3 };
-    ($gpio:ident, spi_miso) => { $gpio.pin4 };
-    ($gpio:ident, spi_ss0) => { $gpio.pin2 };
+    ($gpio:ident, spi0_sck) => { $gpio.pin5 };
+    ($gpio:ident, spi0_mosi) => { $gpio.pin3 };
+    ($gpio:ident, spi0_miso) => { $gpio.pin4 };
+    ($gpio:ident, spi0_ss0) => { $gpio.pin2 };
     // spi_ss1 is not documented
-    ($gpio:ident, spi_ss2) => { $gpio.pin9 };
-    ($gpio:ident, spi_cs3) => { $gpio.pin10 };
+    ($gpio:ident, spi0_ss2) => { $gpio.pin9 };
+    ($gpio:ident, spi0_cs3) => { $gpio.pin10 };
     // i2c
-    ($gpio:ident, i2c_sda) => { $gpio.pin12 };
-    ($gpio:ident, i2c_scl) => { $gpio.pin13 };
+    ($gpio:ident, i2c0_sda) => { $gpio.pin12 };
+    ($gpio:ident, i2c0_scl) => { $gpio.pin13 };
     // serial
-    ($gpio:ident, serial_tx) => { $gpio.pin17 };
-    ($gpio:ident, serial_rx) => { $gpio.pin16 };
+    ($gpio:ident, uart0_tx) => { $gpio.pin17 };
+    ($gpio:ident, uart0_rx) => { $gpio.pin16 };
     // digital/physical
     ($gpio:ident, dig0) => { $gpio.pin16 };
     ($gpio:ident, dig1) => { $gpio.pin17 };
@@ -61,15 +61,15 @@ macro_rules! pin {
 /// # Mappings
 /// 
 ///   - `none` — Returns `()` for empty pin if needed in tuple
-///   - `spi_<x>` — SPI pins where `<x>` is one of (`sck`, `mosi`, `miso`, `ss0`, `ss2`, `ss3`)
-///   - `i2c_<x>` — I2C pins where `<x>` is one of (`sda`, `scl`)
-///   - `serial_<x>` — Serial pins where <x> is one of (`tx`, `rx`)
+///   - `spi0_<x>` — SPI pins where `<x>` is one of (`sck`, `mosi`, `miso`, `ss0`, `ss2`, `ss3`)
+///   - `i2c0_<x>` — I2C pins where `<x>` is one of (`sda`, `scl`)
+///   - `uart0_<x>` — Serial pins where <x> is one of (`tx`, `rx`)
 ///   - `dig#` — Digital/physical pins on the board where `#` is from range 0..19
 /// 
 /// # Example
 /// 
 /// ```
-/// let (mosi, miso, sck, cs) = pins!(gpio, (spi_mosi, spi_miso, spi_sck, spi_ss0));
+/// let (mosi, miso, sck, cs) = pins!(gpio, (spi0_mosi, spi0_miso, spi0_sck, spi0_ss0));
 /// // (gpio.pin3, gpio.pin4, gpio.pin5, gpio.pin2)
 /// ```
 /// 
